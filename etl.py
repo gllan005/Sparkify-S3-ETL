9e659548e5a612f4c39cddcba950fa36a4b51780
Gabriel Llanes <gllanes005@outlook.com>
2020-06-18 12:42:03 -0400

    import etl dependencies

---
+++ etl.py
@@ -0,0 +1,7 @@
+#import dependencies
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import year, month, hour, dayofmonth, weekofyear, date_format
