dc2f7a30a10bb900f9907f5b3c568445de563656
Gabriel Llanes <gllanes005@outlook.com>
2020-06-18 16:44:28 -0400

    Add create_spark_session function

--- etl.py
+++ etl.py
@@ -5,3 +5,25 @@ import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, hour, dayofmonth, weekofyear, date_format
+
+#declare config variable with the ConfigParser() funtion from the configparser class
+config = configparser.ConfigParser()
+config.read('dl.cfg') #read in the dl configuration file
+
+# set aws credential global variables by calling objects in the dl config file
+os.environ['AWS_ACCESS_KEY_ID'] = config['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_KEY'] = config['AWS_SECRET_KEY']
+
+#function to create spark session
+
+def create_spark_session():
+    spark = SparkSession \
+        .builder \
+        .appName('Data lake') \
+        .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.0') \
+        .getOrCreate()
+
+
+
+
+def process_song_data(input, output)
